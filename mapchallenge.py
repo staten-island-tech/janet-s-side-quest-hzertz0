a3d77ad80bec6172503415e02ae316f229d70149
hzertz0 <darwenzhu1221@gmail.com>
2025-03-06 09:54:10 -0500

    whywhywhywhywhywhywhywhywhywehywhywhyhwywhywhwyhwyhwhwhywhywhy

---
+++ mapchallenge.py
@@ -0,0 +1,13 @@
+temperatures = ["Label", 32, 50, 77, 104]
+def f2c(temperatures):
+    answer = {}
+
+    for num in temperatures[1:]:
+        calculate = map(int, num[1:])
+
+    return 
+
+
+
+
+
